4f67786d57a9fc10b1f23f0092ddeb38f076e98e
Alvar Pietro Sebastian Manfé <91345538+alvarmanfe@users.noreply.github.com>
2026-05-06 20:08:21 +0300

    Update vite.config.ts

--- vko3/vite.config.ts
+++ vko3/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Cloud-services-in-web-development/',
+  base: '/Cloud-services-in-web-development/vko3/',
 });
